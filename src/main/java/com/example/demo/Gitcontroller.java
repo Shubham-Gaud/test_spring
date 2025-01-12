0ab9751fbe0e799a34674f4b38eefaa8e1923aa9
Shubham-Gaud <shubhamgaud18@gmail.com>
2025-01-12 16:54:34 +0530

    rebase test commit

--- src/main/java/com/example/demo/Gitcontroller.java
+++ src/main/java/com/example/demo/Gitcontroller.java
@@ -5,13 +5,14 @@ public class Gitcontroller {
 	
 	public static void main(String[] args) {
 		System.out.println("Test");
-<<<<<<< HEAD
+
 		
 		System.out.println("***********");
 		
 		int b =20;
-=======
->>>>>>> parent of 01d10a6 (Thirst branch commit)
+		
+		int f =40;
+
 	}
 
 }
