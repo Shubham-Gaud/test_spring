24476fe395da799d6c3dee098effc7b6866f5e72
Shubham-Gaud <shubhamgaud18@gmail.com>
2025-01-12 16:52:31 +0530

    Test

--- src/main/java/com/example/demo/Gitcontroller.java
+++ src/main/java/com/example/demo/Gitcontroller.java
@@ -5,10 +5,13 @@ public class Gitcontroller {
 	
 	public static void main(String[] args) {
 		System.out.println("Test");
+<<<<<<< HEAD
 		
 		System.out.println("***********");
 		
 		int b =20;
+=======
+>>>>>>> parent of 01d10a6 (Thirst branch commit)
 	}
 
 }
